73f37f4481884f3461d4d64ce43a104821daed01
lyonsam <lyonsam@cobalt>
2007-01-15 00:42:05 -0600

    added include GraphCorrelations.hpp and changed parameter names

--- include/xaif_interface.hpp
+++ include/xaif_interface.hpp
@@ -7,6 +7,7 @@
 
 #include "angel_types.hpp"
 
+#include "xaifBooster/algorithms/CrossCountryInterface/inc/GraphCorrelations.hpp"
 #include "xaifBooster/algorithms/CrossCountryInterface/inc/LinearizedComputationalGraph.hpp"
 #include "xaifBooster/algorithms/CrossCountryInterface/inc/JacobianAccumulationExpressionList.hpp"
 
@@ -15,8 +16,8 @@ namespace angel {
 void compute_partial_elimination_sequence (const xaifBoosterCrossCountryInterface::LinearizedComputationalGraph& xgraph,
 				           int tasks, 
 				           double, // for interface unification
-				           xaifBoosterCrossCountryInterface::JacobianAccumulationExpressionList& expression_list,
-					   xaifBoosterCrossCountryInterface::LinearizedComputationalGraph& rgraph,
+				           xaifBoosterCrossCountryInterface::JacobianAccumulationExpressionList& jae_list,
+					   xaifBoosterCrossCountryInterface::LinearizedComputationalGraph& rg,
 					   xaifBoosterCrossCountryInterface::VertexCorrelationList& v_cor_list,
 					   xaifBoosterCrossCountryInterface::EdgeCorrelationList& e_cor_list);
 
